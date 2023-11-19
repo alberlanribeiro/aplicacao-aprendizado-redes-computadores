4c102d1492eb37b88cc676a339083cd9629fb70c
Alberlan Ribeiro <130510099+alberlanribeiro@users.noreply.github.com>
2023-11-19 19:27:33 -0300

    Initial commit

---
+++ corpoperguntas/script.js
@@ -0,0 +1,38 @@
+//array contendo as respostas corretas
+let corretas = [3,1,2,2,3];
+
+//array para guardar as respostas que o usuário selecionou
+let opcao_escolhida=[];
+
+let quantidade_corretas=0;
+
+//função que pega o numero da pergunta e o input escolhido dessa pergunta
+
+function resposta(num_pergunta, selecionada){
+    //guarda a resposta escolhida
+    opcao_escolhida[num_pergunta] = selecionada.value;
+
+    //o seguinte codigo é para por na cor branco o fundo dos inputs para quando escolher outra opção 
+    //configura o id para selecionar a seção correspondente
+    id="p" + num_pergunta;
+
+    labels = document.getElementById(id).childNodes;
+    labels[3].style.backgroundColor = "white";
+    labels[5].style.backgroundColor = "white";
+    labels[7].style.backgroundColor = "white";
+
+    //doy el color a label seleccionado
+    selecionada.parentNode.style.backgroundColor = "#cec0fc";
+
+}
+
+
+function corrigir(){
+    quantidade_corretas=0;
+        for(i=0; i < corretas.length; i++){
+                if(corretas[i] == opcao_escolhida[i]){
+                    quantidade_corretas++;
+                }   
+        }
+        document.getElementById("resultado").innerHTML = quantidade_corretas;
+}
