8ffd18c64b73d574fe3fe783eff2620953d2557b
Alberlan Ribeiro <130510099+alberlanribeiro@users.noreply.github.com>
2023-12-11 23:38:31 -0300

    tentativa de menu lateral

---
+++ scripts/script_botao_menu.js
@@ -0,0 +1,11 @@
+
+
+
+function botao_menu(){
+    document.getElementById("botao").style.display = "block";
+    var dropdown = window.document.getElementById("botao")
+
+    dropdown.innerHTML += '<p><a href="">O que é uma rede de computadores?</a></p>'
+
+    
+}
